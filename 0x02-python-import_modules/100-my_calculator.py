090dc7e2389e279fcb5e3bf197e577ce1a901928
Harobed1724 <deborahakoranteng@gmail.com>
2022-06-03 10:08:27 +0000

    Edited

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,22 +3,21 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    num_args = len(sys.argv)
-    if num_args != 4:
+    argv = sys.argv
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
-
-    if op is '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op is '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op is '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op is '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+        sys.exit(1)
+    elif argv[2] == '+':
+        op = add
+    elif argv[2] == '-':
+        op = sub
+    elif argv[2] == '*':
+        op = mul
+    elif argv[2] == '/':
+        op = div
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
+
+    result = op(int(argv[1]), int(argv[3]))
+    print("{} {} {} = {}".format(int(argv[1]), argv[2], int(argv[3]), result))
